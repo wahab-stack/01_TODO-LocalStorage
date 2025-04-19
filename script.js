65e93471981f7072117e964ec4244420f3cde703
Wahab1437 <wahab.suit997@gmail.com>
2025-04-19 12:16:03 +0500

    ToDo List local storage

---
+++ script.js
@@ -0,0 +1,19 @@
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let tasks = [];
+
+addTaskButton.addEventListener("click", () => {
+  const taskText = todoInput.value.trim();
+  if (taskText === "") return;
+
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  };
+  tasks.push(newTask);
+  todoInput.value = ""; // clear input
+  console.log(tasks);
+});
