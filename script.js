4c0bd011c4e4fcc600eca8220d74672c3bee86a9
Wahab1437 <wahab.suit997@gmail.com>
2025-04-23 00:18:52 +0500

    script completed

--- script.js
+++ script.js
@@ -1,62 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const todoInput = document.getElementById("todo-input");
-//   const addTaskButton = document.getElementById("add-task-btn");
-//   const todoList = document.getElementById("todo-list");
-
-//   let tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
-
-//   tasks.forEach((task) => renderTask(task));
-
-//   addTaskButton.addEventListener("click", () => {
-//     const taskText = todoInput.value.trim();
-//     if (taskText === "") return;
-
-//     const newTask = {
-//       id: Date.now(),
-//       text: taskText,
-//       completed: false,
-//     };
-//     tasks.push(newTask);
-//     saveTasks();
-//     todoInput.value = ""; // clear input
-//     console.log(tasks);
-//   });
-
-//   // todoInput.addEventListener("keypress", function (e) {
-//   //   if (e.key === "Enter") {
-//   //     addTaskButton.click();
-//   //   }
-//   // });
-
-//   function renderTask(task) {
-//     const li = document.createElement("li");
-//     li.setAttribute("data-id", task.id);
-//     li.innerHTML = `
-//     <span>${task.text}</span>
-//     <button>delete</button>
-//     `;
-
-//     li.addEventListener("click", (e) => {
-//       if (e.target.tagName === "BUTTON") return;
-//       task.completed = !task.completed;
-//       li.classList.toggle("completed");
-//       saveTasks();
-//     });
-
-//     li.querySelector("button").addEventListener("click", (e) => {
-//       e.stopPropagation(); //Prevent toggle from firing
-//       tasks = tasks.filter((t) => t.id !== task.id);
-//       li.remove();
-//       saveTasks();
-//     });
-//     todoList.appendChild(li);
-//   }
-
-//   function saveTasks() {
-//     localStorage.setItem("Tasks", JSON.stringify(tasks));
-//   }
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   const todoInput = document.getElementById("todo-input");
   const addTaskBtn = document.getElementById("add-task-btn");
@@ -95,3 +36,66 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+/*
+
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
+
+  let tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+
+  tasks.forEach((task) => renderTask(task));
+
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    todoInput.value = ""; // clear input
+    console.log(tasks);
+  });
+
+  // todoInput.addEventListener("keypress", function (e) {
+  //   if (e.key === "Enter") {
+  //     addTaskButton.click();
+  //   }
+  // });
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>
+    `;
+
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation(); //Prevent toggle from firing
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+    todoList.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("Tasks", JSON.stringify(tasks));
+  }
+});
+
+*/
